e886819e52211ac7a9334337457b4ff50617f6b8
almtav08 <al375846>
2021-01-10 20:37:06 +0100

    prueba integración hub

--- Hub/src/app/devices/device.service.spec.ts
+++ Hub/src/app/devices/device.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { Subject } from 'rxjs';
 import { STORAGE, ILocalStorage } from '../localstorage/i-local-storage';
 import { LocalStorageService } from '../localstorage/localstorage.service';
@@ -20,10 +20,11 @@ describe('DeviceService', () => {
   beforeEach(() => {
     [mockServer, topicoServidor] = creaMockServerService();
     //[mockStorage,topicoStorage] = creaMockLocalStorageService();
+    mockStorage = creaMockLocalStorageService();
     TestBed.configureTestingModule({
       providers: [
         { provide: SERVER_SERVICE, useValue: mockServer },
-        { provide: STORAGE, useClass:LocalStorageService },
+        { provide: STORAGE, useValue: mockStorage },
       ],
     });
 
@@ -33,17 +34,17 @@ describe('DeviceService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-  it('Debería de cambiar el estado de los dispositivos que le indica el servidor', async () => {
-    let aux = service.getDevices().pipe(take(2),toArray()).toPromise()
+  it('Debería de cambiar el estado de los dispositivos que le indica el servidor', fakeAsync (() => {
+
     topicoServidor.next(
       {turnOn:['MCU'],
     turnOff:[]}
     )
-    let devices = await aux
-    let device = devices[1].find(d=>d.id == 'MCU')
-    expect(device?.state).toEqual('ON')
-   
-  });
+    tick()
+
+   expect(mockStorage.updateDevice.calls.count()).toEqual(1);
+   expect(mockStorage.updateDevice.calls.first().args[0].id).toEqual('MCU');
+  }));
 });
 function creaMockServerService(): [
   jasmine.SpyObj<IServer>,
@@ -55,9 +56,12 @@ function creaMockServerService(): [
   mock.setDeviceList.and.returnValue();
   return [mock, topicoServidor];
 }
-function creaMockLocalStorageService(): [jasmine.SpyObj<ILocalStorage>,Subject<Device>] {
-  const topicoStorage = new Subject<Device>();
-  const mock = jasmine.createSpyObj('LocalStorageService', ['getDevices']);
-  mock.getDevices.and.returnValue(topicoStorage.asObservable());
-  return [mock,topicoStorage];
+function creaMockLocalStorageService(): jasmine.SpyObj<ILocalStorage> {
+  const mock = jasmine.createSpyObj('LocalStorageService', ['updateDevice', 'addDevice', 'getDevice', 'getDevices']);
+  mock.updateDevice.and.returnValue(new Promise<boolean>((resolve) => {resolve(true)}))
+  mock.addDevice.and.returnValue(new Promise<string>((resolve) => {resolve('MCU')}))
+  mock.getDevice.and.returnValue(new Promise<Device>((resolve) => {resolve({id: 'MCU', type:DeviceType.ALARMA, state:'ON', turned:true})}))
+  const s = new Subject<Device>();
+  mock.getDevices.and.returnValue(s.asObservable())
+  return mock;
 }
